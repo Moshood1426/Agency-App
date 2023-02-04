b218332c1cb16fbe774a1e47629c5f039a1b4a9c
Moshood1426 <moshood.yemi10@gmail.com>
2023-02-04 17:23:26 -0500

    banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Banner from "./components/banner";
+import Header from "./components/header";
 import "./styles/App.scss";
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    //Set css varible vh
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
   return (
-    <div className='App'>
-      <h2>Hello this is your starter files </h2>
+    <div className="App">
+      <Header />
+      <Banner />
     </div>
   );
 }
